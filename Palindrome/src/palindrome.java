cb050461d7ececb5598437b23ddd134d9abe6da7
Brandon <baubrey91@gmail.com>
2017-07-12 15:43:15 -0700

    week 1

---
+++ Palindrome/src/palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class palindrome {
+	
+	public static void main(String[] args) {
+		System.out.println(checkPalindrome("a man a plan a canal panama"));
+	}
+	
+	public static boolean checkPalindrome(String s){
+		if(s==null||s.length()==0) return false;
+ 
+		s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+ 
+		for(int i = 0; i < s.length() / 2 ; i++){
+			if(s.charAt(i) != s.charAt(s.length() - 1 - i)){
+				return false;
+			}
+		}
+ 
+		return true;
+	}
+}
